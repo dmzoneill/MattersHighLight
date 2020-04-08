c5b37a116c4f60ead01442f6642a735b1b61a67b
dmzoneill <david.o.neill@canonical.com>
2020-04-08 11:21:20 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,25 @@
+// this is the background code...
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete') {
+
+                chrome.tabs.executeScript(tab.ib, {
+                        file: 'jq.js'
+                });
+
+                chrome.tabs.executeScript(tab.ib, {
+                        file: 'inject.js'
+                });
+        }
+})
+
+// listen for our browerAction to be clicked
+chrome.browserAction.onClicked.addListener(function (tab) {
+        // for the current tab, inject the "inject.js" file & execute it
+        chrome.tabs.executeScript(tab.ib, {
+                file: 'jq.js'
+        });
+        chrome.tabs.executeScript(tab.ib, {
+                file: 'inject.js'
+        });
+});
---
+++ inject.js
@@ -0,0 +1,302 @@
+// this is the code which will be injected into a given page...
+
+var eodlog = [];
+var pastes = [];
+var pagerDuty = [];
+var salesforce = [];
+
+
+function setup() {
+
+	$("a[id=action-link]").click(function () {
+
+		var title = $(this).closest('td').prev('td').text();
+		var customer = $(this).closest('td').prev('td').prev('td').prev('td').text();
+
+		var id = $(this).attr('class');
+		var ticket_id = id.split('-');
+		var sfid = ticket_id[2];
+
+		console.log(title)
+		console.log(customer)
+		console.log(sfid)
+
+		$("#submitForm" + sfid).click(function () {
+			var id = $(this).attr('id');
+			var ticket_id = id.split('m');
+			var sfid = ticket_id[2];
+
+			var comment_made = $("#comment_" + sfid).val();
+			var status_made = $("#status_" + sfid).val();
+			var severity_made = $("#severity_" + sfid).val();
+
+			addToLog("Triaged " + sfid, sfid, [customer, title, comment_made, status_made, severity_made]);
+		});
+
+		addToLog("clicked " + sfid, sfid, [customer, title]);
+	});
+
+	$(".btn-primary").click(function () {
+
+		var title = $(".page-header").find('a:first').text();
+		var customer = $(".page-header").find('div:first').text();
+		var idp = window.location.href.split('/');
+		var sfid = idp[idp.length - 1];
+
+		console.log(title)
+		console.log(customer)
+		console.log(sfid)
+
+		addToLog("Action " + sfid, sfid, [customer, title]);
+	});
+
+	$(".ack-incidents").click(function () {
+
+		$('.ember-checkbox:checkbox:checked').each(function () {
+			var status = $(this).closest('td').next('td').text().trim();
+			var urgency = $(this).closest('td').next('td').next('td').text().trim();
+			var title = $(this).closest('td').next('td').next('td').next('td').text().trim();
+			var create = $(this).closest('td').next('td').next('td').next('td').next('td').text().trim();
+
+			addToPagerDuty("Marked Acknowledged", [status, urgency, title, create]);
+		});
+	});
+
+	$(".resolve-incidents").click(function () {
+
+		$('.ember-checkbox:checkbox:checked').each(function () {
+			var status = $(this).closest('td').next('td').text().trim();
+			var urgency = $(this).closest('td').next('td').next('td').text().trim();
+			var title = $(this).closest('td').next('td').next('td').next('td').text().trim();
+			var create = $(this).closest('td').next('td').next('td').next('td').next('td').text().trim();
+
+			addToPagerDuty("Marked Resolved", [status, urgency, title, create]);
+		});
+	});
+
+	$(".snooze-incidents").click(function () {
+
+		$('.ember-checkbox:checkbox:checked').each(function () {
+			var status = $(this).closest('td').next('td').text().trim();
+			var urgency = $(this).closest('td').next('td').next('td').text().trim();
+			var title = $(this).closest('td').next('td').next('td').next('td').text().trim();
+			var create = $(this).closest('td').next('td').next('td').next('td').next('td').text().trim();
+			addToPagerDuty("Marked Snoozed", [status, urgency, title, create]);
+		});
+	});
+}
+
+function addToLog(message, sfid, additional) {
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0');
+	var yyyy = today.getFullYear();
+
+	var timenow = new Date();
+	var timestr = timenow.getHours() + ":" + timenow.getMinutes()
+
+	today = dd + '/' + mm + '/' + yyyy;
+	var todaylog = { 'date': today, 'time': timestr, 'action': message, 'sfid': sfid };
+
+	if (additional != undefined) {
+		todaylog.extra = additional;
+	}
+
+	eodlog.push(todaylog);
+
+	saveLog();
+}
+
+function addToPagerDuty(message, additional) {
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0');
+	var yyyy = today.getFullYear();
+
+	var timenow = new Date();
+	var timestr = timenow.getHours() + ":" + timenow.getMinutes()
+
+	today = dd + '/' + mm + '/' + yyyy;
+	var todaylog = { 'date': today, 'time': timestr, 'action': message };
+
+	if (additional != undefined) {
+		todaylog.extra = additional;
+	}
+
+	pagerDuty.push(todaylog);
+
+	savePagerDuty();
+}
+
+
+function logPaste(from, url) {
+	pastes.push([from, url]);
+	savePastes();
+}
+
+function logSF(title, url) {
+	salesforce.push([title, url]);
+	saveSF();
+}
+
+
+function saveLog() {
+	var myJSON = JSON.stringify(eodlog);
+
+	console.log(myJSON);
+
+	chrome.storage.local.set({ log: myJSON }, function () {
+		console.log('Value is set to ' + myJSON);
+		printLog();
+	});
+}
+
+function savePastes() {
+	var myJSON = JSON.stringify(pastes);
+
+	chrome.storage.local.set({ pastes: myJSON }, function () {
+		console.log('Value is set to ' + myJSON);
+	});
+}
+
+function loadLog() {
+	console.log("load log");
+	chrome.storage.local.get(['log'], function (result) {
+		console.log(result)
+		if (result.hasOwnProperty("log")) {
+			eodlog = JSON.parse(result.log);
+			printLog();
+		}
+		else {
+			console.log("log was empty");
+		}
+
+		if (window.location.href.indexOf("https://portal.admin.canonical.com/bootstack/cases") != -1) {
+			var title = $(".page-header").find('a:first').text();
+			var customer = $(".page-header").find('div:first').text();
+			var idp = window.location.href.split('/');
+			var sfid = idp[idp.length - 1];
+			if(sfid!=""){
+				addToLog("Reviewed " + sfid, sfid, [customer, title]);
+			}			
+		}
+	});
+}
+
+function loadPagerDuty() {
+	chrome.storage.local.get(['pagerDuty'], function (result) {
+		pagerDuty = JSON.parse(result.pagerDuty);
+		console.log('Value read from log as ' + pagerDuty);
+	});
+}
+
+function savePagerDuty() {
+	var myJSON = JSON.stringify(pagerDuty);
+
+	chrome.storage.local.set({ pagerDuty: myJSON }, function () {
+		console.log('Value is set to ' + myJSON);
+	});
+}
+
+function saveSF() {
+	var myJSON = JSON.stringify(salesforce);
+
+	chrome.storage.local.set({ salesforce: myJSON }, function () {
+		console.log('Value is set to ' + myJSON);
+	});
+}
+
+function loadPastes(title, url) {
+	chrome.storage.local.get(['pastes'], function (result) {
+		if (typeof (result.pastes) === "undefined") {
+			// do nothing
+		}
+		else {
+			pastes = JSON.parse(result.pastes);
+			console.log('Value read from log as ' + pastes);
+		}
+
+		if (title != undefined) {
+			logPaste(title, url);
+		}
+	});
+}
+
+function loadSF(title, url) {
+	chrome.storage.local.get(['salesforce'], function (result) {
+		if (typeof (result.salesforce) === "undefined") {
+			// do nothing
+		}
+		else {
+			salesforce = JSON.parse(result.salesforce);
+			console.log('Value read from log as ' + salesforce);
+		}
+		if (title != undefined) {
+			logSF(title, url);
+		}
+	});
+}
+
+function printLog() {
+	for (i = 0; i < eodlog.length; i++) {
+		console.log(eodlog[i]);
+	}
+}
+
+function mapCustomerNamesToTicketHeader() {
+	$(".table-condensed > tbody  > tr").each(function() {
+	     var ticketId = $(this).find("th").eq(0).text();
+	     var customer = $(this).find("td").eq(0).text();
+	     console.log(ticketId + " - " + customer);
+             var newheader = $("#triageCase"+ticketId).find("h4").eq(0).html() + " - " + customer;
+	     $("#triageCase"+ticketId).find("h4").eq(0).html(newheader);
+	
+	     if(customer.trim() == "Delta Dental") {
+		$(this).hide();
+             }
+	});
+}
+
+
+(function () {
+
+	if (window.location.href.indexOf("https://portal.admin.canonical.com/incident/event") != -1) {
+		var ele = document.getElementById("review-approve-event").style.display;
+		if (ele == "none") {
+			console.log("already approved, end of queue or direct linked");
+			return;
+		}
+
+		document.getElementById("review-approve-event").click();
+		document.location.href = document.getElementById("next-review-event").getAttribute('href');
+		console.log("clicked");
+	}
+
+	if (window.location.href.indexOf("https://portal.admin.canonical.com/bootstack/triage/") != -1) {
+		loadLog();
+		setup();
+		mapCustomerNamesToTicketHeader();
+	}
+
+	if (window.location.href.indexOf("https://portal.admin.canonical.com/bootstack/cases") != -1) {
+		loadLog();
+		setup();
+	}
+
+	if (window.location.href.indexOf("https://pastebin.canonical.com/p/") != -1) {
+		loadPastes($("h1:eq( 1 )").text(), window.location.href);
+	}
+
+	if (window.location.href.indexOf("https://canonical.pagerduty.com/incident") != -1) {
+		loadPagerDuty();
+		setup();
+	}
+
+	if (window.location.href.indexOf("https://canonical.my.salesforce.com/") != -1) {
+		//setup();
+		var parts = window.location.href.split('/');
+		if (parts.length == 4) {
+			loadSF($(".efhpTitle").text(), window.location.href);
+		}
+	}
+})();
