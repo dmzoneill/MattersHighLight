5201d8ab1c3d8ce0b60269d5a310de9b204513c8
dmzoneill <dmz.oneill@gmail.com>
2020-08-11 09:00:07 +0100

    shift N P

--- inject.js
+++ inject.js
@@ -627,4 +627,23 @@ $(document).ready(function () {
 			});
 		});
 	}
+
+	$(document).on("keypress", function(e) { 
+		if ( e.shiftKey && ( e.key == "N" ) ) {
+			var next = $("#favoriteChannelList > li.active").next();
+			var hyper = $(next).children("a")[0];
+			setTimeout(function(){ hyper.click();}, 100);
+			e.preventDefault();
+        	return false;
+		}
+		if ( e.shiftKey && ( e.key == "P" ) ) {
+			var prev = $("#favoriteChannelList > li.active").prev();
+			var hyper = $(prev).children("a")[0];
+			setTimeout(function(){ hyper.click();}, 100);
+			e.preventDefault();
+        	return false;
+		}
+	});
 });
+
+
